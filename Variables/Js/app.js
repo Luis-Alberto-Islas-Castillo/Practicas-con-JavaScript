52b4636916dbd479dee9445e4759101986a205b4
Luis Alberto <luisalbertoislascastillo@gmail.com>
2019-03-22 13:43:37 -0600

    Create app.js

---
+++ Variables/Js/app.js
@@ -0,0 +1,18 @@
+"use strict"
+
+var nombre = "Luis";
+
+console.log(nombre)
+
+function saludo() {
+    let nombre = 'yacafx';
+        console.log(nombre)
+
+    let edad = 34;
+        console.log(edad)
+}
+
+saludo();
+
+
+const pi = 3.1416;
