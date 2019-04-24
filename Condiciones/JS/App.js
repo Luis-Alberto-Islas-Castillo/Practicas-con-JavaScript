b48aa5b9414673586d0c6f488203f908de6e193b
Luis Alberto <luisalbertoislascastillo@gmail.com>
2019-04-24 03:57:27 -0500

    Create App.js

---
+++ Condiciones/JS/App.js
@@ -0,0 +1,16 @@
+"use strict"
+
+// Condicion if-else
+
+var datoA = 10;
+var datoB = 20;
+var resultado = "Sin datos";
+
+if(datoA > datoB){
+    //resultado = datoA+datoB; 
+    resultado = "La condicion se cumplio ";
+} else {
+    resultado = "La condicion no se cumplio";
+}
+
+console.log("El resultado es: ", resultado);
